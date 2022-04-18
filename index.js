9fe36de5344c4b07b96cb48609a855f7be486de0
iulpv <yul_karpva@mail.ru>
2022-04-18 11:49:01 +0500

    1-2

---
+++ index.js
@@ -0,0 +1,23 @@
+let countCoffe = 1;
+let newForm
+const button = document.querySelector(".add-button")
+button.addEventListener("click", function () {
+
+
+    let forms = document.querySelector(".beverage");
+    newForm = forms.cloneNode(true);
+    const count = newForm.querySelector('.beverage-count')
+    countCoffe += 1
+    count.textContent = `Напиток №${countCoffe}`
+    forms.before(newForm)
+});
+
+function remove(target) {
+    if (countCoffe > 1)
+        target.parentElement.remove();
+    countCoffe -= 1
+}
+// cloneText = (textarea) =>{
+//     text = textarea.value.replace(/(срочно)|(быстрее)|(побыстрее)|(скорее)|(поскорее)|(очень нужно)/gi,"<b>$&</b>");
+//     textarea.parentNode.querySelector("span").innerHTML = text;
+// }
